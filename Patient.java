8bcce8e2037c2dc96b6d1c95b6c7826cfda81c23
Abdul Rehman <mehars.6925@gmail.com>
2019-07-06 19:47:50 +0500

    updated

--- Patient.java
+++ Patient.java
@@ -1,17 +1,28 @@
 public class Patient extends Role {
     private String disease;
 
-    public Patient(Person person, String disease) {
-        super(person);
+    public Patient(Clinic clinic, Person person, String disease) {
+        super(clinic, person);
+        this.setClinic(clinic);
         this.setDisease(disease);
+        clinic.addPatient(this);
     }
 
     public void setDisease(String disease) {
         this.disease = disease;
     }
 
+    public void addAppointment(Appointment appointment) {
+        if (!super.appointments.contains(appointment))
+            super.appointments.add(appointment);
+    }
+
     public String getDisease() {
         return this.disease;
+    }
+
+    public Appointment[] getAppointments() {
+        return (Appointment[]) super.appointments.toArray();
     }
 
     public String getRole() {
