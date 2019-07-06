2c8a9268ca23af7a3fc31c79c1276a3dcac579d0
Abdul Rehman <mehars.6925@gmail.com>
2019-07-06 19:46:56 +0500

    logic to add appointment

--- Doctor.java
+++ Doctor.java
@@ -1,16 +1,17 @@
 import java.util.List;
+import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Doctor extends Role {
     private String specialization;
-    private List<Appointment> appointments;
     private List<Availability> availabilities;
 
-    public Doctor(Person person, String specialization) {
-        super(person);
+    public Doctor(Clinic clinic, Person person, String specialization) {
+        super(clinic, person);
         this.setSpecialization(specialization);
-        this.appointments = new ArrayList<Appointment>();
+        super.appointments = new ArrayList<Appointment>();
         this.availabilities = new ArrayList<Availability>();
+        clinic.addDoctor(this);
     }
 
     public void setSpecialization(String specialization) {
@@ -18,7 +19,34 @@ public class Doctor extends Role {
     }
 
     public void addAppointment(Appointment appointment) {
-        this.appointments.add(appointment);
+        boolean available = false;
+        Iterator<Availability> itr = this.availabilities.iterator();
+
+        while(itr.hasNext()) {
+            Availability availability = itr.next();
+            if(
+                availability.getDay().matches(appointment.getDay()) &&
+                availability.getTimeIn() - appointment.getTimeStart() > 0 &&
+                availability.getTimeOut() - appointment.getTimeEnd() < 0
+            ) available = true;
+        }
+
+        if (available && !super.appointments.contains(appointment)) 
+            super.appointments.add(appointment);
+
+    }
+
+    public void addAvailability(Availability availability) {
+        if (!this.availabilities.contains(availability))
+            this.availabilities.add(availability);
+    }
+
+    public Appointment[] getAppointments() {
+        return (Appointment[]) super.appointments.toArray();
+    }
+
+    public Availability[] getAvailabilities() {
+        return (Availability[]) this.availabilities.toArray();
     }
 
     public String getSpecialization() {
