bde744fe006415ce9275e655af7a1120ea97806c
Abdul Rehman <mehars.6925@gmail.com>
2019-07-06 19:48:38 +0500

    updated

--- Role.java
+++ Role.java
@@ -1,14 +1,24 @@
 public abstract class Role {
+    private Clinic clinic;
     private Person person;
+    protected List<Appointment> appointments;
 
-    public Role(Person person) {
+    public Role(Clinic clinic, Person person) {
         this.person = person;
+        this.clinic = clinic;
     }
 
-    public getPerson() {
+    public Clinic getClinic() {
+        return this.clinic;
+    }
+
+    public Person getPerson() {
         return this.person;
     }
 
     public abstract String getRole();
+    
+    public abstract void addAppointment();
 
+    public abstract Appointment[] getAppointments();
 }
